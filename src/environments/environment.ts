a134d335dce4d38e8239a0eb2f5792d7a0773d60
rezakhoy <reza.khoy@gmail.com>
2023-07-08 11:37:21 +0330

    مرحله اول تغییر رمز توسط کاربر

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 // export const API_URL = 'http://192.168.9.18:8080/';
 // export const API_URL = 'http://192.168.1.2:8080/';
 // export const API_URL = 'http://2.187.249.48:8080/';
-export const API_URL = 'http://89.37.12.242:8080/';
+// export const API_URL = 'http://89.37.12.242:8080/';
 export const permission = 'user';
 export const owner = 'سکه و آبشده حمزه نژاد';
-// export const API_URL = 'http://2.187.249.48:8080/';
+export const API_URL = 'http://2.187.249.48:8080/';
 
 // export const owner = 'سکه و آبشده امینی';
 // export const API_URL = 'http://178.131.19.218:8080/';
